616e78ebef261a60e89e3eb2c32aef9b8f3b5584
Артём <hugochacasser@gmail.com>
2023-03-03 02:41:40 -0900

    Add Configuration for Authorization
    Used IdentityServer4

---
+++ Notes.Identity/Data/AuthDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Notes.Identity.Models;
+
+namespace Notes.Identity.Data;
+
+public class AuthDbContext : IdentityDbContext<AppUser>
+{
+    public AuthDbContext(DbContextOptions<AuthDbContext> options) :
+        base(options){}
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<AppUser>(entity => entity.ToTable(name: "Users"));
+        builder.Entity<IdentityRole>(entity => entity.ToTable(name : "Roles"));
+        builder.Entity<IdentityUserRole<string>>(entity =>
+            entity.ToTable(name: "UserRole"));
+        builder.Entity<IdentityUserClaim<string>>(entity =>
+            entity.ToTable("UserClaim"));
+        builder.Entity<IdentityUserLogin<string>>(entity =>
+            entity.ToTable("UserLogins"));
+        builder.Entity<IdentityUserToken<string>>(entity =>
+            entity.ToTable("UserTokens"));
+        builder.Entity<IdentityRoleClaim<string>>(entity =>
+            entity.ToTable("RoleClaims"));
+
+        builder.ApplyConfiguration(new AppUserConfiguration());
+    }
+}
--- Notes.Identity/Program.cs
+++ Notes.Identity/Program.cs
@@ -1,6 +1,32 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Notes.Identity;
+using Notes.Identity.Data;
 
-app.MapGet("/", () => "Hello World!");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+       var host = CreateHostBuilder(args).Build();
+       using (var scope = host.Services.CreateScope())
+       {
+           var serviceProvider = scope.ServiceProvider;
+           try
+           {
+               var context = serviceProvider.GetRequiredService<AuthDbContext>();
+               DbInitializer.Initialize(context);
+           }
+           catch (Exception ex)
+           {
+               var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+               logger.LogError(ex, "An error occured while app initialization");
+           }
+       }
+       host.Run();
+    }
 
-app.Run();
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<StartUp>();
+            });
+}
